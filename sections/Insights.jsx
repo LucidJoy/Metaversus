fe314a182e2930a7fdb7ea5bd6fc303a0475bef1
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-12-14 12:01:01 +0530

    World section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,13 @@
-'use client';
+"use client";
 
-const Insights = () => (
-  <section>
-    Insights section
-  </section>
-);
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { exploreWorlds } from "../constants";
+
+const Insights = () => <section>Insights section</section>;
 
 export default Insights;
